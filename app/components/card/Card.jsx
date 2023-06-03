c485877066b7d1694f4dd8353253c608c91d68b8
AvotraIsmael <avotraismael4@gmail.com>
2023-06-03 10:23:41 +0100

    fix next image component

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,46 +1,47 @@
-import React from "react";
-import "./Card.css";
-import { BiTimeFive } from "react-icons/bi";
-import { IoStarSharp, IoAdd } from "react-icons/io5";
-import { MdFavoriteBorder } from "react-icons/md";
+import React from 'react'
+import './Card.css'
+import { BiTimeFive } from 'react-icons/bi'
+import { IoStarSharp, IoAdd } from 'react-icons/io5'
+import { MdFavoriteBorder } from 'react-icons/md'
+import Image from 'next/image'
 
 const Card = ({ image, title }) => {
   return (
-    <div className="recipe-card">
-      <div className="top">
-        <div className="image">
-          <img src={image} alt={title} />
+    <div className='recipe-card'>
+      <div className='top'>
+        <div className='image'>
+          <Image src={image} alt={title} fill />
           <MdFavoriteBorder />
         </div>
       </div>
-      <div className="bottom">
-        <div className="title-star">
+      <div className='bottom'>
+        <div className='title-star'>
           <h4>{title.slice(0, 50)}</h4>
-          <div className="star">
+          <div className='star'>
             <span>
               <IoStarSharp />
             </span>
             <p>4.5</p>
           </div>
         </div>
-        <div className="food_info">
-          <div className="time">
+        <div className='food_info'>
+          <div className='time'>
             <BiTimeFive />
             <p>30 min</p>
           </div>
-          <div className="calories">
+          <div className='calories'>
             <p>450 Kcal</p>
           </div>
         </div>
-        <div className="price-add">
+        <div className='price-add'>
           <p>50 DH</p>
           <button>
-            Add <IoAdd />{" "}
+            Add <IoAdd />{' '}
           </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
